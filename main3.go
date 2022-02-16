c4aaf63f7a06a3e02fe8ffed9aebedd927f75325
Shrishti <shrishti.singh076@gmail.com>
2022-02-16 13:03:21 +0530

    Learning Go

--- main3.go
+++ main3.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 import (
@@ -44,50 +43,3 @@ func main() {
 		log.Fatalln(err)
 	}
 }
-=======
-package main
-
-import (
-	"log"
-	"os"
-	"text/template"
-)
-
-var tpl *template.Template
-
-type hotel struct {
-	Name    string
-	Address string
-	city    string
-	zip     string
-	region  string
-}
-type hotels []hotel
-
-func init() {
-	tpl = template.Must(template.ParseFiles("index2.gohtml"))
-}
-
-func main() {
-	h := hotels{
-		hotel{
-			Name:    "Broadway",
-			Address: "India",
-			city:    "Maharastra",
-			zip:     "562101",
-			region:  "southern",
-		},
-		hotel{
-			Name:    "Cadway",
-			Address: "America",
-			city:    "New York",
-			zip:     "705401",
-			region:  "nothern",
-		},
-	}
-	err := tpl.Execute(os.Stdout, h)
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
->>>>>>> e30cc05... Learning Go and my first server
