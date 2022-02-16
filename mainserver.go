8f73788683957f8351e69269e1f6c09203965e05
Shrishti <shrishti.singh076@gmail.com>
2022-02-16 12:58:30 +0530

    Go

--- mainserver.go
+++ mainserver.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package main
 
 import (
@@ -33,3 +34,35 @@ func main() {
 //open cmd
 //type-> telnet localhost 8080
 //(8080 or any other port mentioned in the program)
+=======
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(conn, "\n Hello I am the tcp server")
+		fmt.Fprintln(conn, "\nHow's your day?")
+		fmt.Fprintf(conn, "%v", "Good day boss!")
+
+		conn.Close()
+	}
+}
+>>>>>>> e30cc05... Learning Go and my first server
